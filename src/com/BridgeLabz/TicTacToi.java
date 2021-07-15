94fafd9335b8d1edbe4b30a4a6332b1a67aed87c
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-07-15 09:15:09 +0530

    ADD:Board ablity to take x & 0

--- src/com/BridgeLabz/TicTacToi.java
+++ src/com/BridgeLabz/TicTacToi.java
@@ -1,28 +1,48 @@
 package com.BridgeLabz;
 
+import java.util.Scanner;
+
 public class TicTacToi {
 
     //M*N
-    static String a[][] =new String [3][3];
+    static String a[][] = new String[3][3];
 
     //Printing Tic Toe Board To See  Input From User
 
-    void   printboard(String a[][])
-
-    {
-        System.out.println("------------------------");
-        System.out.println("| " +a[0][0] + " | " + a[0][1] + " | " + a[0][2] + " |");
+    void printboard(String a[][]) {
+        System.out.println("--------------------");
+        System.out.println("| " + a[0][0] + " | " + a[0][1] + " | " + a[0][2] + " |");
         System.out.println("|---------------|");
-        System.out.println("| " +a[1][0] + " | "+ a[1][1] + " | " + a[1][2] + " |");
+        System.out.println("| " + a[1][0] + " | " + a[1][1] + " | " + a[1][2] + " |");
         System.out.println("|---------------|");
-        System.out.println("| " +a[2][0] + " | "+ a[2][1] + " | " + a[2][2] + " |");
+        System.out.println("| " + a[2][0] + " | " + a[2][1] + " | " + a[2][2] + " |");
         System.out.println("|---------------|");
     }
 
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int i, j, i1, j1, flag = 0;
+
+        for (int ir = 0; ir < 3; ir++)
+        {
+            for (int jc = 0; jc < 3; jc++)  //to initializ blank space
+            {
+                a[ir][jc] = " ";
+            }
+
+        }
+        System.out.println("player 1 is X");
+        System.out.println("Player 2 is O");
+        System.out.println("Let's start the game ");
+        TicTacToi ob = new TicTacToi();
+        System.out.println();
+       ob.printboard(a);
 
 
 
 
+    }
 
 
 }
